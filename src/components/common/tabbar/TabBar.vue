db7e6dd8415c4b6af331626743adeb541c872ddd
wxx <562391493@qq.com>
2021-04-07 11:51:07 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="tab-bar">
+      <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name:'TabBar',
+}
+</script>
+<style>
+ #tab-bar {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    background-color: #f6f6f6;
+    box-shadow: 0 -2px 1px rgba(105, 103, 103, 0.1);
+  }
+</style>
