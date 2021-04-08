3eaedc372bada2c9117f750e20e150756c12d27c
wxx <562391493@qq.com>
2021-04-08 14:21:04 +0800

    轮播图和导航栏

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <swiper>
+            <swiper-item v-for="(item,index) in banners" :key="index">
+                <a :href="item.link"></a>
+                <img :src="item.image" alt="">
+            </swiper-item>
+        </swiper>
+    </div>
+</template>
+<script>
+import { Swiper , SwiperItem } from 'components/common/swiper';
+export default {
+    nmae: 'HomeSwiper',
+    props:{
+        banners:{
+            type:Array,
+            default() {
+                return []
+                }
+        }
+    },
+    components:{
+        Swiper,
+        SwiperItem
+    }
+}
+</script>
+<style scoped>
+
+</style>
